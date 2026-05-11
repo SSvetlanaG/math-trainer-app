31fed17c4168f59e6e0e01e40f9f487b1f4516e8
nipferd21@gmail.com <your_email@example.com>
2026-05-11 15:42:31 +0600

    Нина: добавлена арифметика и зафиксирован main

--- main.py
+++ main.py
@@ -7,20 +7,19 @@ def main():
         print("\n=== МАТЕМАТИЧЕСКИЙ ТРЕНАЖЕР ===")
         print("1. Арифметика (числа)")
         print("2. Матрицы (сумма, умножение)")
-        print("3. Посмотреть статистику") # Новый пункт
+        print("3. Посмотреть статистику")  # Новый пункт
         print("0. Выход")
-        
-        choice = input("\nВыберите раздел: ")
-        
-        if choice == "1":
-    		score = arithmetic.start_trainer() # Вызов функции Нины
-    		stats.save_result("Игрок", score) # Сразу сохраняем результат
-	elif choice == "2":
-    		score = matrices.start_trainer() # Вызов функции Даниила
-    		stats.save_result("Игрок", score)
 
+        choice = input("\nВыберите раздел: ")
+
+        if choice == "1":
+            score = arithmetic.start_trainer()  # Вызов функции Нины
+            stats.save_result("Игрок", score)  # Сразу сохраняем результат
+        elif choice == "2":
+            score = matrices.start_trainer()  # Вызов функции Даниила
+            stats.save_result("Игрок", score)
         elif choice == "3":
-            stats.show_stats() # Вызов функции из модуля stats.py
+            stats.show_stats()  # Вызов функции из модуля stats.py
         elif choice == "0":
             print("Программа завершена. Удачи!")
             break
