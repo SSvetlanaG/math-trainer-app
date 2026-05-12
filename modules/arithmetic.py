2ad7ed917a8bd0b51b0081ff9d80dd140310dfad
nina2101 <112973121+nina2101@users.noreply.github.com>
2026-05-12 23:18:03 +0600

    Update arithmetic.py

---
+++ modules/arithmetic.py
@@ -0,0 +1,32 @@
+import random
+
+def start_trainer():
+    print("\n--- ТРЕНАЖЕР: АРИФМЕТИКА ---")
+    
+    a = random.randint(1, 20)
+    b = random.randint(1, 20)
+    
+    
+    operation = random.choice(['+', '-', '*'])
+    
+    if operation == '+':
+        correct_answer = a + b
+        question = f"Сколько будет {a} + {b}? "
+    elif operation == '-':
+        correct_answer = a - b
+        question = f"Сколько будет {a} - {b}? "
+    else:  
+        correct_answer = a * b
+        question = f"Сколько будет {a} * {b}? "
+    
+    try:
+        user_answer = int(input(question))
+        if user_answer == correct_answer:
+            print("✅ Правильно!")
+            return 1
+        else:
+            print(f"❌ Ошибка! Правильный ответ: {correct_answer}")
+            return 0
+    except ValueError:
+        print("❌ Нужно вводить числа!")
+        return 0
