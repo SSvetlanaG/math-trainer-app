c7643b66a0332a97e194ca50998349a4a2ef7695
nipferd21@gmail.com <your_email@example.com>
2026-05-12 10:04:46 +0600

    Added basic arithmetic trainer logic

--- modules/arithmetic.py
+++ modules/arithmetic.py
@@ -2,17 +2,27 @@ import random
 
 def start_trainer():
     print("\n--- ТРЕНАЖЕР: АРИФМЕТИКА ---")
+    
     a = random.randint(1, 20)
     b = random.randint(1, 20)
     
+    if random.choice([True, False]):
+        
+        correct_answer = a + b
+        question = f"Сколько будет {a} + {b}? "
+    else:
+        
+        correct_answer = a * b
+        question = f"Сколько будет {a} * {b}? "
+    
     try:
-        user_answer = int(input(f"Сколько будет {a} + {b}? "))
-        if user_answer == a + b:
+        user_answer = int(input(question))
+        if user_answer == correct_answer:
             print("✅ Правильно!")
-            return 1  # Возвращаем 1 балл
+            return 1
         else:
-            print(f"❌ Ошибка! Правильный ответ: {a + b}")
+            print(f"❌ Ошибка! Правильный ответ: {correct_answer}")
             return 0
     except ValueError:
-        print("Нужно вводить числа!")
+        print("❌ Нужно вводить числа!")
         return 0
